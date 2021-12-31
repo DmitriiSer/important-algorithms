3094bc7d88f4021d313e36f47e7da06ea6f37338
DmitriiSer <dmitrii.serikov@gmail.com>
2021-12-30 23:25:39 -0500

    Added isSorted helper method.

--- server/data/algo/util.js
+++ server/data/algo/util.js
@@ -3,5 +3,11 @@ module.exports = {
         const tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
+    },
+    isSorted: (arr, lo = 0, hi = arr.length - 1) => {
+        return arr.every((v, i, a) => {
+            if (i < lo || i > hi || hi - lo === 0) return true;
+            return i === lo || a[i - 1] <= v;
+        });
     }
 };
